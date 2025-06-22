7641217359ce630f05a7b89921431058ec2e6096
Patrick Orave <oravep@gmail.com>
2025-06-22 17:52:42 -0500

    feat: add Article and PageClient components for article rendering

---
+++ apps/pragmatic-papers/src/app/(frontend)/articles/[slug]/page.tsx
@@ -0,0 +1,97 @@
+import type { Metadata } from 'next'
+
+import { PayloadRedirects } from '@/components/PayloadRedirects'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import { draftMode } from 'next/headers'
+import React, { cache } from 'react'
+
+import type { Article } from '@/payload-types'
+
+import { PostHero } from '@/heros/PostHero'
+import { generateMeta } from '@/utilities/generateMeta'
+import PageClient from './page.client'
+import { LivePreviewListener } from '@/components/LivePreviewListener'
+import RichText from '@/components/RichText'
+
+export async function generateStaticParams() {
+  const payload = await getPayload({ config: configPromise })
+  const articles = await payload.find({
+    collection: 'articles',
+    draft: false,
+    limit: 1000,
+    overrideAccess: false,
+    pagination: false,
+    select: {
+      slug: true,
+    },
+  })
+
+  const params = articles.docs.map(({ slug }) => {
+    return { slug }
+  })
+
+  return params
+}
+
+type Args = {
+  params: Promise<{
+    slug?: string
+  }>
+}
+
+export default async function Article({ params: paramsPromise }: Args) {
+  const { isEnabled: draft } = await draftMode()
+  const { slug = '' } = await paramsPromise
+  const url = '/articles/' + slug
+  const article = await queryArticleBySlug({ slug })
+
+  if (!article) return <PayloadRedirects url={url} />
+
+  return (
+    <article className="pt-16 pb-16">
+      <PageClient />
+
+      {/* Allows redirects for valid pages too */}
+      <PayloadRedirects disableNotFound url={url} />
+
+      {draft && <LivePreviewListener />}
+
+      <PostHero post={article} />
+
+      <div className="flex flex-col items-center gap-4 pt-8">
+        <div className="container">
+          <RichText className="max-w-[48rem] mx-auto" data={article.content} enableGutter={false} />
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
+  const { slug = '' } = await paramsPromise
+  const article = await queryArticleBySlug({ slug })
+
+  return generateMeta({ doc: article })
+}
+
+const queryArticleBySlug = cache(async ({ slug }: { slug: string }) => {
+  const { isEnabled: draft } = await draftMode()
+
+  const payload = await getPayload({ config: configPromise })
+
+  const result = await payload.find({
+    collection: 'articles',
+    draft,
+    limit: 1,
+    overrideAccess: draft,
+    pagination: false,
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  })
+
+  return result.docs?.[0] || null
+})
