ae7f175de1d0e61017c2ba37aaa2581dac132096
Patrick Orave <oravep@gmail.com>
2025-06-25 09:29:22 -0500

    fix: tailwindcss configs in squiggle.tsx

--- apps/pragmatic-papers/src/components/ui/squiggle.tsx
+++ apps/pragmatic-papers/src/components/ui/squiggle.tsx
@@ -4,13 +4,13 @@ import * as React from 'react'
 
 const SquiggleStatic: React.FC = () => {
   return (
-    <div className="bg-[url(/squiggle-static.svg)] w-full h-1 mt-6 bg-size-[auto 4px] bg-repeat-x bg-postition-[100% 100%] relative block" />
+    <div className="bg-[url(/squiggle-static.svg)] w-full h-1 mt-6 bg-[length:auto_4px] bg-repeat-x bg-[left_calc(100%)_top_calc(100%)] relative block" />
   )
 }
 
 const Squiggle: React.FC = () => {
   return (
-    <div className="bg-[url(/squiggle.svg)] w-full h-1 mt-6 bg-size-[auto 4px] bg-repeat-x bg-postition-[100% 100%] relative block" />
+    <div className="bg-[url(/squiggle.svg)] w-full h-1 mt-6 bg-[length:auto_4px] bg-repeat-x bg-[left_calc(100%)_top_calc(100%)] relative block" />
   )
 }
 
