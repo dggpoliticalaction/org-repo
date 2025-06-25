f0f847b3745874b4204454b5952bd80da42fda3d
Patrick Orave <oravep@gmail.com>
2025-06-25 08:49:11 -0500

    feat: add Squiggle and SquiggleStatic

---
+++ apps/pragmatic-papers/src/components/ui/squiggle.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { cn } from '@/utilities/ui'
+import * as React from 'react'
+
+const SquiggleStatic: React.FC = () => {
+  return (
+    <div className="bg-[url(/squiggle-static.svg)] w-full h-1 mt-6 bg-size-[auto 4px] bg-repeat-x bg-postition-[100% 100%] relative block" />
+  )
+}
+
+const Squiggle: React.FC = () => {
+  return (
+    <div className="bg-[url(/squiggle.svg)] w-full h-1 mt-6 bg-size-[auto 4px] bg-repeat-x bg-postition-[100% 100%] relative block" />
+  )
+}
+
+export { Squiggle, SquiggleStatic }
