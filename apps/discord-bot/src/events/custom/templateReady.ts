a46acee09a9ee92c31069732e8ccf0c6e1f6946a
Tyler Malmberg <tmalmber@cord.edu>
2025-07-07 23:41:08 -0500

    add git-less repo

---
+++ apps/discord-bot/src/events/custom/templateReady.ts
@@ -0,0 +1,15 @@
+import { Discord, OnCustom } from '@/decorators'
+
+@Discord()
+export default class TemplateReadyEvent {
+
+	// =============================
+	// ========= Handlers ==========
+	// =============================
+
+	@OnCustom('templateReady')
+	async templateReadyHandler() {
+		console.log('the template is fully ready!')
+	}
+
+}
