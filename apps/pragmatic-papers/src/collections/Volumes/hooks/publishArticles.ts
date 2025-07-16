853e5d24cb6f22b1a1955ef306520a68a0825d4f
Patrick Orave <oravep@gmail.com>
2025-07-15 20:55:12 -0500

    Implement publishing child articles

---
+++ apps/pragmatic-papers/src/collections/Volumes/hooks/publishArticles.ts
@@ -0,0 +1,42 @@
+import { type Article, type Volume } from '@/payload-types'
+import { type CollectionBeforeChangeHook } from 'payload'
+
+export const publishArticles: CollectionBeforeChangeHook<Volume> = async ({
+  data,
+  req: { payload },
+}) => {
+  // Automatically publish articles when a volume is published
+  if (data._status === 'published' && data.articles) {
+    // check if articles is an array of ids, then fetch the articles
+    let articles: Article[] = []
+    if (typeof data.articles[0] === 'number') {
+      articles = await Promise.all(
+        data.articles.map(
+          async (articleId) =>
+            await payload.findByID({
+              collection: 'articles',
+              id: articleId as number,
+            }),
+        ),
+      )
+    } else {
+      articles = data.articles as Article[]
+    }
+
+    const articlesToPublish = articles.filter((article) => article._status !== 'published')
+
+    if (articlesToPublish.length > 0) {
+      // Update each article individually
+      for (const article of articlesToPublish) {
+        await payload.update({
+          collection: 'articles',
+          id: typeof article === 'object' ? article.id : article,
+          data: {
+            _status: 'published',
+            publishedAt: new Date().toISOString(),
+          },
+        })
+      }
+    }
+  }
+}
