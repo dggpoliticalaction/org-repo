70ba29fe9ab7f68d302c600a5a9422e354336fab
tkmalmberg <tmalmber@cord.edu>
2025-08-24 13:51:45 -0500

    CTA Reaction Reading with Chart Summary

--- apps/discord-bot/src/triggers/cta-post.ts
+++ apps/discord-bot/src/triggers/cta-post.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
-import { ChannelType, Client, ThreadAutoArchiveDuration, User, type Message } from "discord.js";
+import { ChannelType, Collection, GuildMember, roleMention, ThreadAutoArchiveDuration, type Message } from "discord.js";
 import { type Trigger } from "./trigger";
 import { type EventData } from "../models/internal-models";
-import { BarController, BarElement, CategoryScale, Chart, LinearScale, plugins } from "chart.js";
+import { BarController, Colors, BarElement, CategoryScale, Chart, LinearScale, PieController, ArcElement, Legend, Title } from "chart.js";
 import { Canvas } from "canvas";
 import { writeFile } from "node:fs";
 
@@ -15,7 +15,7 @@ export class CTAPostTrigger implements Trigger {
         const ctaChannel = msg.guild?.channels.cache.find(ctaChannel => ctaChannel?.name === channelName)
         // check cta channel exists
         if (ctaChannel !== undefined) {
-            
+
             // check message came from cta channel
             if (msg.channelId === ctaChannel.id) {
                 return true
@@ -32,21 +32,43 @@ export class CTAPostTrigger implements Trigger {
             [X] Create Public Thread
             [X] Async read reactions to message
                 [X] Get users and roles from reactions
-            [ ] Create chart from reactions sorted by role
+            [X] Create chart from reactions sorted by role
         */
+        const regionRoles = [
+            'Midwest-South Squad',
+            'Northeast Squad',
+            'West Coast Squad'
+        ]
+
+        const finishedEmoji = '✅'
 
         if (msg.channel.type === ChannelType.GuildText) {
-            const userReactions: {[reactId: string]: string[]} = {};
-            const collector = msg.createReactionCollector({time: 15_000});
+            const roleReactions: { [reactId: string]: string[] } = {};
+            const collector = msg.createReactionCollector({ time: 15_000 });
 
-            collector.on('collect', (reaction, user) => {
-                let reactionEmojiName = reaction.emoji?.name ?? '';
-                
-                if (!userReactions[reactionEmojiName]) {
-                    userReactions[reactionEmojiName] = [];
+            collector.on('collect', async (reaction, user) => {
+                const member = msg.guild?.members.fetch(user.id)
+                if (member != undefined) {
+
+                    console.log(`Got ${reaction.emoji.name} from ${(await member).displayName}`)
+
+                    if (reaction.emoji.name === finishedEmoji.toString()) {
+
+                        const memberRoles = (await member).roles.cache;
+                        const role = memberRoles.filter(r => regionRoles.includes(r.name));
+                        const memberRegionRole = role.first()?.name;
+
+                        if (memberRegionRole != undefined) {
+                            if (!roleReactions[memberRegionRole]) {
+                                roleReactions[memberRegionRole] = [];
+                            }
+                            roleReactions[memberRegionRole]?.push(user.id);
+                            console.log(`${role} member, ${user.displayName}`);
+                        }
+
+                        console.log(`${Object.entries(roleReactions)}`)
+                    }
                 }
-                console.log(`${reactionEmojiName} from ${user.displayName}`);
-                userReactions[reactionEmojiName]?.push(user.id);
             });
 
             collector.on('end', collected => {
@@ -57,52 +79,101 @@ export class CTAPostTrigger implements Trigger {
                     reason: 'Tracking CTA participation.'
                 });
 
-                // const reactionSummary = Object.entries(userReactions).map(([emoji, users]) => `${emoji}: ${users.length} users`).join('\n');
-                
-                Chart.register(
-                    CategoryScale,
-                    BarController,
-                    BarElement,
-                    LinearScale,
-                );
-
-                const canvas = new Canvas(800, 600);
-                const chart = new Chart(canvas, {
-                    type: "bar",
-                    data: {
-                        labels: Object.keys(userReactions),
-                        datasets: [{
-                            label: "Reactions",
-                            data: Object.values(userReactions).map(users => users.length),
-                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        }]
-                    }
-                });
-
-                const pngBuffer = canvas.toBuffer('image/png');
-                const attachment = {
-                    files: [{
-                        attachment: pngBuffer,
-                        name: 'reaction_summary.png'
-                    }]
-                };
+                const pngBuffer = this.createChart(roleReactions).toBuffer('image/png');
 
                 writeFile('./misc/tmp/reaction_summary.png', pngBuffer, (err) => {
                     if (err) throw err;
                     console.log('Chart saved');
                 });
-                
-               // (async () => {
-                //     const threadChannel = await thread;
-                //     await threadChannel.send(`Reaction Summary:\n${
-                //         attachment.files[0]?.attachment
-                //     }`);
-                // })();
+
+                (async () => {
+                    const threadChannel = await thread;
+                    await threadChannel.send({ files: ["./misc/tmp/reaction_summary.png"] });
+                })();
             });
-            
+
             console.log(`Data: ${data}`)
             return
         }
+    }
 
+    private createChart(roleReactions: object): Canvas {
+
+        Chart.register(
+            CategoryScale,
+            PieController,
+            ArcElement,
+            BarController,
+            BarElement,
+            LinearScale,
+            Legend,
+            Title,
+            Colors
+        );
+
+        const canvas = new Canvas(800, 600);
+
+        Chart.defaults.color = 'rgb(255,255,255)';
+        // Chart.defaults.borderColor = 'rgb(255,255,255)';
+
+        const plugin = {
+            id: 'customCanvasBackgroundColor',
+            beforeDraw: (chart, args, options) => {
+                const { ctx } = chart;
+                ctx.save();
+                ctx.globalCompositeOperation = 'destination-over';
+                ctx.fillStyle = options.color || 'rgb(0, 0, 0)';
+                ctx.fillRect(0, 0, chart.width, chart.height);
+                ctx.restore();
+            }
+        };
+
+        const _ = new Chart(canvas, {
+            type: 'pie',
+            data: {
+                labels: Object.keys(roleReactions),
+                datasets: [{
+                    label: "Squad CTA Completions by Region",
+                    // roleReactions is a map of <string, string[]>
+                    data: Object.values(roleReactions).map(role => role.length),
+                    backgroundColor: [
+                        'rgb(228, 27, 50)',
+                        'rgb(255, 255, 255)',
+                        'rgb(23, 25, 77)',
+                    ],
+                    borderColor: [
+                        'rgb(0,0,0)',
+                        'rgb(0,0,0)',
+                        'rgb(0,0,0)',
+                    ]
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                        align: 'center',
+                        labels: {
+                            font: {
+                                size: 20
+                            },
+                            boxWidth: 20,
+                            usePointStyle: false
+                        },
+                    },
+                    title: {
+                        display: true,
+                        text: "Squad CTA Completions by Region",
+                        font: {
+                            size: 24,
+                        }
+                    }
+                }
+            },
+            plugins: [plugin]
+        });
+
+        return canvas
     }
 }
