addfafd2ce3c9c31d25c9f571ae795c7e03dfecc
alexander.burke <alexander.j.burke@gmail.com>
2025-10-19 20:10:56 -0400

    Migration for the new config

--- apps/dgg-political-action/src/migrations/index.ts
+++ apps/dgg-political-action/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251019_230422 from './20251019_230422';
+import * as migration_20251020_001030 from './20251020_001030';
 
 export const migrations = [
   {
     up: migration_20251019_230422.up,
     down: migration_20251019_230422.down,
-    name: '20251019_230422'
+    name: '20251019_230422',
+  },
+  {
+    up: migration_20251020_001030.up,
+    down: migration_20251020_001030.down,
+    name: '20251020_001030'
   },
 ];
