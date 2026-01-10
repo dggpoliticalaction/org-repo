f75762885701fb6fd2574f93589088ede5a9a880
tadjh <hello@tadjh.com>
2026-01-10 00:07:12 -0800

    Implement hideClose for custom button

--- apps/pragmatic-papers/src/blocks/OffCanvas/Component.tsx
+++ apps/pragmatic-papers/src/blocks/OffCanvas/Component.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@/components/ui/button'
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
+import { X } from 'lucide-react'
 
 interface OffCanvasBlockProps {
   label: string
@@ -22,11 +24,15 @@ export const OffCanvasBlock: React.FC<OffCanvasBlockProps> = ({ label, icon }) =
           {icon}
         </Button>
       </SheetTrigger>
-      <SheetContent className="w-screen sm:max-w-full">
+      <SheetContent className="w-screen sm:max-w-full" hideClose>
         <SheetHeader>
           <SheetTitle className="sr-only">{label}</SheetTitle>
         </SheetHeader>
         <SheetDescription>Add content here</SheetDescription>
+        <SheetClose className="absolute right-4 top-4 flex items-center gap-1 rounded-sm text-sm font-bold opacity-70 ring-offset-background transition-opacity hover:underline hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <span>Close</span>
+          <X className="h-5 w-5" />
+        </SheetClose>
       </SheetContent>
     </Sheet>
   )
