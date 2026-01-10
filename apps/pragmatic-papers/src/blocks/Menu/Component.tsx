3585ee8f76336866950d582954a11f794da9bc3f
tadjh <hello@tadjh.com>
2026-01-09 16:31:45 -0800

    Improve key

--- apps/pragmatic-papers/src/blocks/Menu/Component.tsx
+++ apps/pragmatic-papers/src/blocks/Menu/Component.tsx
@@ -12,9 +12,9 @@ export const MenuBlock: React.FC<MenuBlockProps> = ({ menu }) => {
   if (!menu) return <div />
   return (
     <nav className="flex items-center gap-3 text-sm">
-      {menu.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
-      })}
+      {menu.map(({ link, id }, index) => (
+        <CMSLink key={id || `menu-item-${index}`} {...link} appearance="link" />
+      ))}
     </nav>
   )
 }
