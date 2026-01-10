2a3e98cf2de2ecbbe9a3b9b63844827d23366b7e
tadjh <hello@tadjh.com>
2026-01-09 16:29:57 -0800

    Restrict editing

--- apps/pragmatic-papers/src/Header/config.ts
+++ apps/pragmatic-papers/src/Header/config.ts
@@ -1,5 +1,6 @@
 import type { GlobalConfig } from 'payload'
 
+import { adminFieldLevel } from '@/access/admins'
 import { menu } from '../blocks/Menu/config'
 import { revalidateHeader } from './hooks/revalidateHeader'
 
@@ -7,6 +8,7 @@ export const Header: GlobalConfig = {
   slug: 'header',
   access: {
     read: () => true,
+    update: adminFieldLevel,
   },
   fields: [
     menu({
