7415bb3534b79b3ada6b53373af7a52f9d6d4f2c
tadjh <hello@tadjh.com>
2026-01-10 00:22:07 -0800

    Add slots for rendering elements before and after the menu

--- apps/pragmatic-papers/src/blocks/Menu/Component.tsx
+++ apps/pragmatic-papers/src/blocks/Menu/Component.tsx
@@ -3,18 +3,29 @@ import React from 'react'
 import type { MenuBlock as MenuBlockType } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
+import { cn } from '@/utilities/ui'
 
-interface MenuBlockProps {
+interface MenuBlockProps extends React.HTMLAttributes<HTMLDivElement> {
   menu?: MenuBlockType
+  renderBefore?: React.ReactNode
+  renderAfter?: React.ReactNode
 }
 
-export const MenuBlock: React.FC<MenuBlockProps> = ({ menu }) => {
-  if (!menu) return <div />
+export const MenuBlock: React.FC<MenuBlockProps> = ({
+  menu,
+  className,
+  renderBefore,
+  renderAfter,
+  ...props
+}) => {
   return (
-    <nav className="flex items-center gap-3 text-sm">
-      {menu.map(({ link, id }, index) => (
-        <CMSLink key={id || `menu-item-${index}`} {...link} appearance="link" />
-      ))}
+    <nav className={cn('flex items-center gap-3 text-sm', className)} {...props}>
+      {renderBefore}
+      {menu &&
+        menu.map(({ link, id }, index) => (
+          <CMSLink key={id || `menu-item-${index}`} {...link} appearance="link" />
+        ))}
+      {renderAfter}
     </nav>
   )
 }
