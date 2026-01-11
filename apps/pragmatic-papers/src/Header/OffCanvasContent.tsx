3ae01034b65f1965ea1f25245da8886196a5be50
TadjhBrooks <tadjh.brooks@gmail.com>
2026-01-11 14:52:21 -0800

    Remove logos from offcanvas

--- apps/pragmatic-papers/src/Header/OffCanvasContent.tsx
+++ apps/pragmatic-papers/src/Header/OffCanvasContent.tsx
@@ -3,17 +3,8 @@ import { getCachedGlobal } from '@/utilities/getGlobals'
 import { TextSearch } from 'lucide-react'
 import { MenuBlock } from '@/blocks/Menu/Component'
 import type { Footer, Header } from '@/payload-types'
-import { Logo } from '@/components/Logo'
 import { ActionButton } from './ActionButton/Component'
 import { Copyright } from './Copyright'
-
-const OffCanvasHeader: React.FC = () => {
-  return (
-    <div className="my-6 flex text-left">
-      <Logo variant="stacked" love />
-    </div>
-  )
-}
 
 export async function OffCanvasContent(): Promise<React.JSX.Element> {
   const { primaryMenu, secondaryMenu, actionButton }: Header = await getCachedGlobal('header', 1)()
@@ -23,7 +14,6 @@ export async function OffCanvasContent(): Promise<React.JSX.Element> {
       label="Menu"
       className="flex min-h-screen flex-col gap-3"
       icon={<TextSearch className="h-5 w-5" />}
-      header={<OffCanvasHeader />}
     >
       <div className="flex flex-1 flex-col gap-2">
         <MenuBlock menu={primaryMenu} variant="stacked" />
@@ -31,7 +21,6 @@ export async function OffCanvasContent(): Promise<React.JSX.Element> {
         <ActionButton {...actionButton} />
       </div>
       <div className="flex flex-col flex-wrap items-start gap-3 sm:flex-row sm:items-end">
-        <Logo variant="stacked" love />
         <MenuBlock menu={navItems} />
         <Copyright className="text-sm">Pragmatic Papers</Copyright>
       </div>
