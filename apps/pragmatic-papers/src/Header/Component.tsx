c60c20eacb19c53a07ac7db05a0a6e70b2dded0b
TadjhBrooks <tadjh.brooks@gmail.com>
2026-01-10 16:14:48 -0800

    Deprecate HeaderClient

--- apps/pragmatic-papers/src/Header/Component.tsx
+++ apps/pragmatic-papers/src/Header/Component.tsx
@@ -1,7 +1,6 @@
 import { MenuBlock } from '@/blocks/Menu/Component'
 import { OffCanvasBlock } from '@/blocks/OffCanvas/Component'
 import { Logo } from '@/components/Logo'
-import { HeaderClient } from '@/Header/Component.client'
 import type { Header } from '@/payload-types'
 import { getCachedGlobal } from '@/utilities/getGlobals'
 import { TextSearch } from 'lucide-react'
@@ -11,7 +10,7 @@ import React from 'react'
 export async function Header(): Promise<React.JSX.Element> {
   const { primaryMenu, secondaryMenu }: Header = await getCachedGlobal('header', 1)()
   return (
-    <HeaderClient className="sticky top-0 z-50 grid grid-cols-[1fr_auto_1fr] items-center gap-4 border-b-2 border-border bg-background px-4 py-6 md:px-6">
+    <header className="sticky top-0 z-50 grid grid-cols-[1fr_auto_1fr] items-center gap-4 border-b-2 border-border bg-background px-4 py-6 md:px-6">
       <MenuBlock menu={primaryMenu} />
       <Link href="/" aria-label="Link to Home" className="inline-flex items-center justify-center">
         <Logo love />
@@ -21,6 +20,6 @@ export async function Header(): Promise<React.JSX.Element> {
         menu={secondaryMenu}
         renderAfter={<OffCanvasBlock label="Menu" icon={<TextSearch className="h-5 w-5" />} />}
       />
-    </HeaderClient>
+    </header>
   )
 }
