e917970f5552f53d75764e56fbf903f6fc9e8b7b
tadjh <hello@tadjh.com>
2026-01-12 15:32:59 -0800

    Add deprecation

--- apps/pragmatic-papers/src/blocks/TwitterEmbed/Component.tsx
+++ apps/pragmatic-papers/src/blocks/TwitterEmbed/Component.tsx
@@ -5,6 +5,8 @@ import type { SocialEmbedBlock as SocialEmbedBlockProps } from '@/payload-types'
  * Legacy block for Twitter embeds.
  * @param props - The props for the Twitter embed block.
  * @returns The Twitter embed component.
+ * @deprecated Use the SocialEmbed block instead.
+ * @see SocialEmbedBlock
  */
 export const TwitterEmbedBlock: React.FC<SocialEmbedBlockProps> = (props) => {
   return <TwitterEmbed {...props} />
