b9038c0ad231c9ce755cb3da3fc1f22b5824d4ae
espdesign <evanpendergraft@gmail.com>
2026-01-12 22:59:31 -0500

    add discord to socialProviders

---
+++ apps/pragmatic-papers/src/utilities/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from 'better-auth'
+
+export const auth = betterAuth({
+  socialProviders: {
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    },
+  },
+})
