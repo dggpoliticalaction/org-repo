78ed9556fd03ed2aadf4eafa1075327859ee3384
alex <alex>
2026-01-19 19:01:49 -0800

    add missing ref

--- apps/pragmatic-papers/src/collections/Articles/index.ts
+++ apps/pragmatic-papers/src/collections/Articles/index.ts
@@ -111,6 +111,7 @@ export const Articles: CollectionConfig = {
                         RedditEmbed,
                         BlueSkyEmbed,
                         TikTokEmbed,
+                        MediaCollageBlock
                       ],
                       inlineBlocks: [InlineMathBlock],
                     }),
