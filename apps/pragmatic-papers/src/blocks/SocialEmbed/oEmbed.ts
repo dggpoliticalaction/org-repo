e7ff18f5f963b2e9bd10dfade51f415a10186b3c
tadjh <hello@tadjh.com>
2026-01-20 00:30:01 -0800

    Create oEmbed.ts

---
+++ apps/pragmatic-papers/src/blocks/SocialEmbed/oEmbed.ts
@@ -0,0 +1,94 @@
+/**
+ * oEmbed TypeScript types based on https://oembed.com/#section2 (oEmbed 1.0)
+ */
+
+/**
+ * The format of the oEmbed response.
+ */
+export type OEmbedFormat = 'json' | 'xml'
+
+/**
+ * The type of the oEmbed resource.
+ */
+export type OEmbedResourceType = 'photo' | 'video' | 'link' | 'rich'
+
+/**
+ * Consumer request query parameters (spec-defined).
+ * Query params are ultimately serialized to strings; this is an ergonomic shape.
+ */
+export interface OEmbedRequestQuery {
+  url: string
+  maxwidth?: number
+  maxheight?: number
+  format?: OEmbedFormat
+
+  // Providers may support custom parameters.
+  [customParam: string]: string | number | undefined
+}
+
+/**
+ * Thumbnail dependency rule from the spec:
+ * if any thumbnail_* is present, all three must be present.
+ */
+export type OEmbedThumbnail =
+  | {
+      thumbnail_url: string
+      thumbnail_width: number
+      thumbnail_height: number
+    }
+  | {
+      thumbnail_url?: undefined
+      thumbnail_width?: undefined
+      thumbnail_height?: undefined
+    }
+
+/**
+ * Common response parameters valid for all response types.
+ * Includes an index signature because providers may add fields not in the spec.
+ */
+export interface OEmbedBase {
+  type: OEmbedResourceType
+  version: '1.0'
+
+  title?: string
+  author_name?: string
+  author_url?: string
+  provider_name?: string
+  provider_url?: string
+  cache_age?: number
+
+  // allow additional provider-specific fields
+  // [key: string]: unknown;
+} // & OEmbedThumbnail;
+
+export type OEmbedPhoto = OEmbedBase & {
+  type: 'photo'
+  url: string
+  width: number
+  height: number
+}
+
+export type OEmbedVideo = OEmbedBase & {
+  type: 'video'
+  html: string
+  width: number
+  height: number
+}
+
+export type OEmbedRich = OEmbedBase & {
+  type: 'rich'
+  html: string
+  width: number
+  height: number
+}
+
+export type OEmbedLink = OEmbedBase & {
+  type: 'link'
+}
+
+export type OEmbedResponse = OEmbedPhoto | OEmbedVideo | OEmbedRich | OEmbedLink
+
+export const isOEmbedPhoto = (x: OEmbedResponse): x is OEmbedPhoto => x.type === 'photo'
+export const isOEmbedVideo = (x: OEmbedResponse): x is OEmbedVideo => x.type === 'video'
+export const isOEmbedRich = (x: OEmbedResponse): x is OEmbedRich => x.type === 'rich'
+export const isOEmbedLink = (x: OEmbedResponse): x is OEmbedLink => x.type === 'link'
