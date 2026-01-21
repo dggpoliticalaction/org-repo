3b895e88c908d01829c891a75953494229a02198
Flargebla <connorzapfel@gmail.com>
2026-01-20 20:04:51 -0500

    Cleaned up the message

--- apps/discord-bot/src/constants/dev-onboarding.ts
+++ apps/discord-bot/src/constants/dev-onboarding.ts
@@ -1,9 +1,30 @@
 export class DevOnboarding {
-    public static Message: string = `So you're interested in the Dev Team...\n
-    Here are the basics of the team:\n
-    * There are 5 projects\n
-    * Weekly meetings are @ 8pm EST\n
-    * Office hours are @ 7pm EST\n
-    
-    Take a look at the [onboarding doc](http://www.google.com) and reach out to @Tyler with any questions\n`
+  public static Title = 'DGGP Developer Onboarding'
+
+  public static Message = `
+    So you're interested in the **DGGP dev team** — awesome! 👋
+
+    Here are the basics:
+
+    **We have five main teams**
+    ▫️ [Pragmatic Papers](https://github.com/dggpoliticalaction/org-repo/tree/dev/apps/pragmatic-papers)
+    ▫️ [DGGP Site](https://github.com/dggpoliticalaction/org-repo/tree/dev/apps/dgg-political-action)
+    ▫️ [Volunteer Management System](https://github.com/dggpoliticalaction/in-house-mgmt)
+    ▫️ [Discord Bot](https://github.com/dggpoliticalaction/org-repo/tree/dev/apps/discord-bot)
+    ▫️ DevOps
+
+    **There are two weekly engagements**
+    ▫️ Team meetings — Wednesdays @ 8pm EST
+    ▫️ Office hours — Tuesdays @ 8pm EST
+    ▫️ Be sure to subscribe to the Discord server events!
+
+    **GitHub access**
+    ▫️ Send your GitHub username to **@Tyler**
+    ▫️ Our monorepo: [org-repo](https://github.com/dggpoliticalaction/org-repo)
+
+    **Google Drive access**
+    ▫️ Send your email address to **@Tyler**
+    ▫️ Review the [Onboarding Doc](https://docs.google.com/document/d/1ftfaatfO1umsE-ZOLmjfohn4JW-3TnSMkZF3ib9AyO0/edit)
+
+    If you have questions, reach out to **@Tyler** or **#dev-team** anytime!`
 }
