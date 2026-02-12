a01424e8e431a70c889cca3b1693b886138ffbfb
alex <alex>
2026-02-12 11:34:03 -0800

    AMAZING WIDTH

--- apps/pragmatic-papers/src/components/ImageModal/index.tsx
+++ apps/pragmatic-papers/src/components/ImageModal/index.tsx
@@ -21,12 +21,12 @@ export const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, resourc
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent 
-        className="max-w-[95vw] max-h-[95vh] p-4 border-0 bg-background"
+        className="max-w-[95vw] max-h-[95vh] p-4 border-0 bg-background w-2/3"
         onPointerDownOutside={onClose}
       >
         {/*We have to set a DialogTitle to stop a acessibility error. The image alt text seems appropriate */}
         <DialogTitle className="sr-only">{alt}</DialogTitle> 
-        <div className="flex flex-col items-center gap-3 h-full overflow-auto">
+        <div className="flex flex-col items-center gap-3 h-full w-auto overflow-auto">
           {children ? (children) : resource ? (
             <>
               {/* eslint-disable-next-line @next/next/no-img-element */}
