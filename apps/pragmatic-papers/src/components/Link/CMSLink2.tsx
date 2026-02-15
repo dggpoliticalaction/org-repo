813d28dcf62093422006fc458e26b6b70e264ca5
TadjhBrooks <tadjh.brooks@gmail.com>
2026-02-14 17:18:07 -0800

    Use slot syntax

--- apps/pragmatic-papers/src/components/Link/CMSLink2.tsx
+++ apps/pragmatic-papers/src/components/Link/CMSLink2.tsx
@@ -1,32 +1,25 @@
 import type { LinkField } from '@/payload-types'
 import { getLinkFieldUrl } from '@/utilities/getLinkFieldUrl'
+import { cn } from '@/utilities/ui'
 import Link from 'next/link'
 
-interface CMSLink2Props extends React.ComponentProps<'a'> {
+interface CMSLinkProps extends React.ComponentProps<'a'> {
   link?: LinkField
 }
 
 // Planning to replace the CMSLink component with this one in the future.
 // Need to add appearance handling before replacing. Use class-variance-authority for handling sizes and variants.
-export const CMSLink: React.FC<CMSLink2Props> = ({ link, children, ...props }) => {
+export const CMSLink: React.FC<CMSLinkProps> = ({ link, className, children, ...props }) => {
   if (!link) return null
   const url = getLinkFieldUrl(link)
   if (!url) return null
-  const sharedProps = {
-    className: 'text-brand underline shadow-none',
-    target: link?.newTab ? '_blank' : undefined,
-    rel: link?.newTab ? 'noopener noreferrer' : undefined,
-    ...props,
-  }
-  const content = children || link?.label
-
-  return link.type === 'custom' ? (
-    <a href={url} {...sharedProps}>
-      {content}
-    </a>
-  ) : (
-    <Link href={url} {...sharedProps}>
-      {content}
-    </Link>
-  )
+  const Slot = link.type === 'custom' ? 'a' : Link
+  return <Slot
+    href={url} className={cn('text-brand underline shadow-none', className)}
+    target={link?.newTab ? '_blank' : undefined}
+    rel={link?.newTab ? 'noopener noreferrer' : undefined}
+    {...props}
+  >
+    {children || link?.label}
+  </Slot>
 }
