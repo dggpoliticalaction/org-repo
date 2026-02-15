9c474374cd16ad6c6f0f6a62476ca1f177731825
TadjhBrooks <tadjh.brooks@gmail.com>
2026-02-15 14:56:06 -0800

    Separate Love Component

--- apps/pragmatic-papers/src/components/Logo/index.tsx
+++ apps/pragmatic-papers/src/components/Logo/index.tsx
@@ -3,20 +3,7 @@ import { LogoStacked } from '@/components/Logo/icons/LogoStacked'
 import { cn } from '@/utilities/ui'
 import { cva, type VariantProps } from 'class-variance-authority'
 import React from 'react'
-import { DggLIcon } from './icons/DggLIcon'
-
-const loveVariants = cva('mr-1 w-auto', {
-  defaultVariants: {
-    variant: 'responsive',
-  },
-  variants: {
-    variant: {
-      inline: 'h-9 sm:h-10 md:h-11 lg:h-12 xl:h-14',
-      stacked: 'h-10 sm:h-11 md:h-12 lg:h-14 xl:h-16',
-      responsive: 'h-10 sm:h-11 md:h-12 lg:h-12 xl:h-14',
-    },
-  },
-})
+import { Love } from './Love'
 
 const logoVariants = cva('w-auto', {
   defaultVariants: {
@@ -37,7 +24,7 @@ const logoVariants = cva('w-auto', {
 
 export interface LogoRootProps
   extends React.SVGProps<SVGSVGElement>,
-    VariantProps<typeof logoVariants> {}
+  VariantProps<typeof logoVariants> { }
 
 export const LogoRoot: React.FC<LogoRootProps> = ({ className, variant, ...props }) => {
   switch (variant) {
@@ -64,7 +51,7 @@ export const LogoRoot: React.FC<LogoRootProps> = ({ className, variant, ...props
 
 export interface LogoProps
   extends React.SVGProps<SVGSVGElement>,
-    VariantProps<typeof logoVariants> {
+  VariantProps<typeof logoVariants> {
   love?: boolean
 }
 
@@ -93,9 +80,7 @@ export interface LogoProps
 export const Logo: React.FC<LogoProps> = ({ love, variant, ...props }) => {
   return (
     <>
-      {love && (
-        <DggLIcon className={cn(loveVariants({ variant }))} aria-hidden={props['aria-hidden']} />
-      )}
+      {love && <Love variant={variant} aria-hidden={props['aria-hidden']} />}
       <LogoRoot className="text-black dark:text-white" variant={variant} {...props} />
     </>
   )
