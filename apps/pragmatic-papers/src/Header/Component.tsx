41c1728126cbf69f978cce243b2d55d29c43b767
TadjhBrooks <tadjh.brooks@gmail.com>
2026-02-14 17:37:10 -0800

    Move towards composition instead of prop drilling

--- apps/pragmatic-papers/src/Header/Component.tsx
+++ apps/pragmatic-papers/src/Header/Component.tsx
@@ -1,11 +1,14 @@
-import { MenuBlockOrEmpty } from '@/blocks/Menu/Component'
+import { MenuBlock, MenuBlockOrEmpty } from '@/blocks/Menu/Component'
 import { Logo } from '@/components/Logo'
+import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { ActionButton } from '@/Header/ActionButton/Component'
-import { OffCanvasContent } from '@/Header/OffCanvasContent'
 import type { Header } from '@/payload-types'
 import { getCachedGlobal } from '@/utilities/getGlobals'
+import { TextSearch, User } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import { Copyright } from './Copyright'
 
 /**
  * Renders the site header including primary and secondary menus, logo, and off-canvas menu.
@@ -19,20 +22,50 @@ import React from 'react'
  */
 export async function Header(): Promise<React.JSX.Element> {
   const { navItems, actionButton }: Header = await getCachedGlobal('header', 1)()
+  const footerData = await getCachedGlobal('footer', 1)()
+
   return (
     <header className="border-border bg-background sticky top-0 z-50 border-b-2 py-6">
       <div className="container grid grid-cols-[1fr_auto_1fr] items-center gap-4">
-        <OffCanvasContent />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="link" size="clear" className="items-center gap-2">
+              <TextSearch className="h-7 w-7" />
+              <span className="sr-only">Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent className='w-full sm:w-3/4'>
+            <div className="flex flex-1 flex-col gap-2">
+              <MenuBlock menu={navItems} variant="stacked" />
+            </div>
+            <div className="flex flex-col flex-wrap items-start gap-3 sm:flex-row sm:items-end">
+              <MenuBlock menu={footerData.navItems} />
+              <Copyright className="text-sm">Pragmatic Papers</Copyright>
+            </div>
+          </SheetContent>
+        </Sheet>
         <Link
           href="/"
           aria-label="Link to Home"
           className="inline-flex items-center justify-center"
         >
-          <Logo love aria-hidden="true" />
+          <Logo aria-hidden="true" />
         </Link>
-        <ActionButton
-          {...actionButton}
-        />
+        <div className='flex items-center gap-2 justify-end'>
+          <ActionButton button={actionButton} />
+          <Button className='hidden md:block'>Log In</Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="link" size="clear" className="md:hidden">
+                <User className="h-7 w-7" />
+                <span className="sr-only">Account</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent className='w-full sm:w-3/4' side="right">
+              <ActionButton button={actionButton} />
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
       <MenuBlockOrEmpty
         className="justify-self-start [&>*]:hidden md:[&>*]:inline-flex"
