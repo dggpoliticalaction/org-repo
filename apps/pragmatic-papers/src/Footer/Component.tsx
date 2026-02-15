371d81400fe098a0db6e9cc619fe30734c1e27c6
TadjhBrooks <tadjh.brooks@gmail.com>
2026-02-15 15:50:51 -0800

    Update footer with new MenuBlock and new Logo

--- apps/pragmatic-papers/src/Footer/Component.tsx
+++ apps/pragmatic-papers/src/Footer/Component.tsx
@@ -1,33 +1,23 @@
+import { MenuBlock } from '@/blocks/Menu/Component'
+import { Logo } from '@/components/Logo'
+import type { Footer } from '@/payload-types'
+import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 import { getCachedGlobal } from '@/utilities/getGlobals'
 import Link from 'next/link'
-import React from 'react'
 
-import type { Footer } from '@/payload-types'
-
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
-import { CMSLink } from '@/components/Link'
-import { Logo } from '@/components/Logo/Logo'
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function Footer() {
+export async function Footer(): Promise<React.ReactElement> {
   const footerData: Footer = await getCachedGlobal('footer', 1)()
-
   const navItems = footerData?.navItems || []
 
   return (
-    <footer className="mt-auto border-t border-border text-black dark:text-white">
-      <div className="container flex flex-col gap-8 py-8 md:flex-row md:justify-between">
+    <footer className="mt-auto">
+      <div className="container flex flex-col space-y-3 border-t border-border py-8 md:flex-row md:justify-between">
         <Link className="flex items-center" href="/">
-          <Logo size="xs" />
+          <Logo love />
         </Link>
-
-        <div className="flex flex-col-reverse items-start gap-4 md:flex-row md:items-center">
+        <div className="flex flex-col-reverse items-start space-y-3 md:flex-row md:items-center">
           <ThemeSelector />
-          <nav className="flex flex-col gap-4 md:flex-row">
-            {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-black dark:text-white" key={i} {...link} />
-            })}
-          </nav>
+          <MenuBlock menu={navItems} />
         </div>
       </div>
     </footer>
