7f474d661dca57b7ba54af46a7ee298cad903ab7
tadjh <hello@tadjh.com>
2026-03-03 16:31:14 -0800

    Patch migrations

--- apps/pragmatic-papers/src/migrations/index.ts
+++ apps/pragmatic-papers/src/migrations/index.ts
@@ -12,8 +12,8 @@ import * as migration_20260110_021152_discord_header_button from './20260110_021
 import * as migration_20260111_055750_slug_field_update from './20260111_055750_slug_field_update';
 import * as migration_20260117_020339_footnotes from './20260117_020339_footnotes';
 import * as migration_20260217_013724_header_footer_redesign from './20260217_013724_header_footer_redesign';
-import * as migration_20260226_021046_feature_authors from './20260226_021046_feature_authors';
 import * as migration_20260303_233515_auto_generate_volume_title from './20260303_233515_auto_generate_volume_title';
+import * as migration_20260304_002934_feature_authors from './20260304_002934_feature_authors';
 
 export const migrations = [
   {
@@ -87,13 +87,13 @@ export const migrations = [
     name: '20260217_013724_header_footer_redesign',
   },
   {
-    up: migration_20260226_021046_feature_authors.up,
-    down: migration_20260226_021046_feature_authors.down,
-    name: '20260226_021046_feature_authors',
-  },
-  {
     up: migration_20260303_233515_auto_generate_volume_title.up,
     down: migration_20260303_233515_auto_generate_volume_title.down,
     name: '20260303_233515_auto_generate_volume_title',
+  },
+  {
+    up: migration_20260304_002934_feature_authors.up,
+    down: migration_20260304_002934_feature_authors.down,
+    name: '20260304_002934_feature_authors'
   },
 ];
