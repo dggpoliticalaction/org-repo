6a77624169d9f2c898dc22c69180033bb07bae92
Ted Timbrell <ted@timbrell.dev>
2026-03-08 00:00:44 -0800

    remove optional chaining

--- apps/pragmatic-papers/src/app/(frontend)/layout.tsx
+++ apps/pragmatic-papers/src/app/(frontend)/layout.tsx
@@ -75,7 +75,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <Footer />
         </Providers>
       </body>
-      {googleAnalyticsId && <GoogleAnalytics gaId={googleAnalyticsId} />}
+      <GoogleAnalytics gaId={googleAnalyticsId} />
     </html>
   )
 }
