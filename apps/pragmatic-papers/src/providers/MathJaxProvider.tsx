63c320dc31f08dd5257cbda885726e8667bce23f
tadjh <hello@tadjh.com>
2026-03-09 18:40:39 -0700

    Restore next/dynamic
    Co-Authored-By: Ted Timbrell <ted@timbrell.dev>

--- apps/pragmatic-papers/src/providers/MathJaxProvider.tsx
+++ apps/pragmatic-papers/src/providers/MathJaxProvider.tsx
@@ -1,4 +1,11 @@
-import { MathJaxContext } from 'better-react-mathjax'
+import dynamic from 'next/dynamic'
+
+const MathJaxContext = dynamic(
+  () => import('better-react-mathjax').then((mod) => mod.MathJaxContext),
+  {
+    ssr: true,
+  },
+)
 
 export const MathJaxProvider: React.FC<{
   enableMathRendering: boolean | null | undefined
