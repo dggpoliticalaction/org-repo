00d913e7ee94d968fb8fc28eac6ee1daa8489e4b
tadjh <hello@tadjh.com>
2026-03-21 11:38:45 -0700

    Disable default bylines in some layouts. Now CMS controlled.

--- apps/pragmatic-papers/src/migrations/index.ts
+++ apps/pragmatic-papers/src/migrations/index.ts
@@ -24,6 +24,7 @@ import * as migration_20260317_033620_add_collections_again_thanks_esp from './2
 import * as migration_20260318_125059_revert_volume_autogen from './20260318_125059_revert_volume_autogen';
 import * as migration_20260318_152547_use_populate_authors_duh from './20260318_152547_use_populate_authors_duh';
 import * as migration_20260318_172936_topics_views from './20260318_172936_topics_views';
+import * as migration_20260321_181233_add_showbyline_to_collection_grid_slots from './20260321_181233_add_showbyline_to_collection_grid_slots';
 
 export const migrations = [
   {
@@ -154,6 +155,11 @@ export const migrations = [
   {
     up: migration_20260318_172936_topics_views.up,
     down: migration_20260318_172936_topics_views.down,
-    name: '20260318_172936_topics_views'
+    name: '20260318_172936_topics_views',
+  },
+  {
+    up: migration_20260321_181233_add_showbyline_to_collection_grid_slots.up,
+    down: migration_20260321_181233_add_showbyline_to_collection_grid_slots.down,
+    name: '20260321_181233_add_showbyline_to_collection_grid_slots'
   },
 ];
