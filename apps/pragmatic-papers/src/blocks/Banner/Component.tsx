38f8bddadf16b86bd6c65b397346c467fb12537b
tadjh <hello@tadjh.com>
2026-03-23 02:13:11 -0700

    Update Component.tsx

--- apps/pragmatic-papers/src/blocks/Banner/Component.tsx
+++ apps/pragmatic-papers/src/blocks/Banner/Component.tsx
@@ -1,8 +1,8 @@
-import type { BannerBlock as BannerBlockProps } from 'src/payload-types'
+import type { BannerBlock as BannerBlockProps } from "src/payload-types"
 
-import RichText from '@/components/RichText'
-import { cn } from '@/utilities/utils'
-import React from 'react'
+import RichText from "@/components/RichText"
+import { cn } from "@/utilities/utils"
+import React from "react"
 
 type Props = {
   className?: string
@@ -10,13 +10,13 @@ type Props = {
 
 export const BannerBlock: React.FC<Props> = ({ className, content, style }) => {
   return (
-    <div className={cn('mx-auto my-8 w-full', className)}>
+    <div className={cn("mx-auto my-8 w-full", className)}>
       <div
-        className={cn('flex items-center rounded-sm border px-6 py-3', {
-          'border-border bg-card': style === 'info',
-          'border-error bg-error/30': style === 'error',
-          'border-success bg-success/30': style === 'success',
-          'border-warning bg-warning/30': style === 'warning',
+        className={cn("flex items-center rounded-sm border px-6 py-3", {
+          "border-border bg-card": style === "info",
+          "border-error bg-error/30": style === "error",
+          "border-success bg-success/30": style === "success",
+          "border-warning bg-warning/30": style === "warning",
         })}
       >
         <RichText data={content} enableGutter={false} enableProse={false} />
