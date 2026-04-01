2c87af8f1279865a6eac74671b12ed73c4844e20
tadjh <hello@tadjh.com>
2026-04-01 13:29:16 -0700

    Apply suggestion from @tadjh

--- apps/pragmatic-papers/next.config.js
+++ apps/pragmatic-papers/next.config.js
@@ -2,7 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload"
 
 import redirects from "./redirects.js"
 
-const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8000"
 
 const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 
