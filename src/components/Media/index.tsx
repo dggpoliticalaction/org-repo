a2945a035c5d3da52e27b39234fc542defeec073
tadjh <hello@tadjh.com>
2026-04-26 10:53:37 -0700

    Update index.tsx

--- src/components/Media/index.tsx
+++ src/components/Media/index.tsx
@@ -1,35 +1,24 @@
 import type { Media as MediaType } from "@/payload-types"
 import React from "react"
 
-import { AudioMedia, type AudioMediaProps } from "./AudioMedia"
 import { ImageMedia, type ImageMediaProps } from "./ImageMedia"
 import { VideoMedia, type VideoMediaProps } from "./VideoMedia"
 
-type MediaTypes = VideoMediaProps | ImageMediaProps | AudioMediaProps
-
-function isVideoMedia(props: MediaTypes): props is VideoMediaProps {
-  return props.media.mimeType?.startsWith("video") ?? false
-}
-
-function isAudioMedia(props: MediaTypes): props is AudioMediaProps {
-  if (!props.media || typeof props.media === "number") return false
-  return props.media.mimeType?.startsWith("audio") ?? false
+function isVideoMediaProps(props: VideoMediaProps | ImageMediaProps): props is VideoMediaProps {
+  if (!props.media) return false
+  if (typeof props.media === "number") return false
+  return props.media.mimeType?.includes("video") ?? false
 }
 
 export function isMedia(media: number | MediaType | undefined | null): media is MediaType {
-  if (!media || typeof media === "number") return false
+  if (!media) return false
+  if (typeof media === "number") return false
   return true
 }
 
-export const Media: React.FC<MediaTypes> = (props) => {
-  if (!props.media || typeof props.media === "number") return null
-
-  if (isVideoMedia(props)) {
+export const Media: React.FC<VideoMediaProps | ImageMediaProps> = (props) => {
+  if (isVideoMediaProps(props)) {
     return <VideoMedia {...props} />
-  }
-
-  if (isAudioMedia(props)) {
-    return <AudioMedia {...props} />
   }
 
   return <ImageMedia {...props} />
